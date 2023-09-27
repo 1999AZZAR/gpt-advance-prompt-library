dd274e1e0c2bafa320332b671609819312f51960
azzar <azzar.mr.zs@gmail.com>
2023-09-27 21:26:42 +0700

    😒

--- web/script/copy_paste.js
+++ web/script/copy_paste.js
@@ -2,14 +2,17 @@
 
 // Function to copy code to the clipboard
 function copyCode() {
-    const codeElement = document.querySelector("article");
-    const text = codeElement.textContent || codeElement.innerText;
-    const textArea = document.createElement("textarea");
-    textArea.value = text;
-    document.body.appendChild(textArea);
-    textArea.select();
-    document.execCommand("copy");
-    document.body.removeChild(textArea);
-    alert("Code copied to clipboard!");
-}
+    var clickedButton = event.target;
+    var codeContainer = clickedButton.parentElement.querySelector('article');
 
+    if (codeContainer) {
+        var textToCopy = codeContainer.textContent;
+        var dummyTextArea = document.createElement('textarea');
+        dummyTextArea.value = textToCopy;
+        document.body.appendChild(dummyTextArea);
+        dummyTextArea.select();
+        document.execCommand('copy');
+        document.body.removeChild(dummyTextArea);
+        alert('Copied text: ' + textToCopy);
+    }
+}
